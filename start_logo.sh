906d58e56f7eed263039dd7729232309b2362914
b.meta <metasound00@gmail.com>
2021-05-05 18:03:24 +0700

    add start logo

---
+++ start_logo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo ifconfig wlan0 up
+wpa_cli -i wlan0 reconnect 
+sudo systemctl stop fingerscan.service
+sleep 10
+sudo systemctl start fingerscan.service
+sleep 30
+pkill -o chromium
+
