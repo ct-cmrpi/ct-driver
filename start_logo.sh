75df35d1f9b0463e836982fca1af2c23eb54fc77
b.meta <metasound00@gmail.com>
2021-05-05 18:26:54 +0700

    play logo

--- start_logo.sh
+++ start_logo.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-chromium-browser --kiosk file:/home/pi/ct-driver/utility/loading_gif/gif_player.html
 sudo ifconfig wlan0 up &&
 wpa_cli -i wlan0 reconnect && 
 sudo systemctl stop fingerscan.service &&
