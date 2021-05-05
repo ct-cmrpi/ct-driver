77120439ff9b6c97843a3c5949f9d0001636e449
b.meta <metasound00@gmail.com>
2021-05-05 11:54:54 +0700

    update sh

--- install_utility.sh
+++ install_utility.sh
@@ -5,8 +5,8 @@ sudo apt-get -y install qml-module-qtquick-* &&
 sudo apt-get -y install matchbox-keyboard && 
 sudo apt-get -y install xfonts-thai &&
 sudo apt -y install libasound2-dev &&
-sudo cp /home/pi/ct-driver/utility/keyboard/Executable/matchbox-keyboard /usr/local/bin && sudo chmod +x /usr/local/bin/matchbox-keyboard &&
+sudo cp /home/pi/ct-driver/utility/keyboard/Executable/matchbox-keyboard /usr/local/bin && 
+sudo chmod +x /usr/local/bin/matchbox-keyboard &&
 sudo cp -r /home/pi/ct-driver/utility/keyboard/Configuration/matchbox-keyboard /usr/local/share &&
-
 sudo cp /home/pi/ct-driver/utility/fonts/browalia.ttc /usr/share/fonts/truetype/ &&
-sudo cp /home/pi/ct-driver/utility/fonts/DB\ Helvethaica\ X\ v3.2.ttf /usr/share/fonts/truetype/ &&
+sudo cp /home/pi/ct-driver/utility/fonts/DB\ Helvethaica\ X\ v3.2.ttf /usr/share/fonts/truetype/
