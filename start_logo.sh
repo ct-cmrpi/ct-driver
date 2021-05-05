94505cf4c9302bca5cd31f713c3bb2e3121c0173
b.meta <metasound00@gmail.com>
2021-05-05 19:28:42 +0700

    fix bug

--- start_logo.sh
+++ start_logo.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-chromium-browser --kiosk file:/home/pi/ct-driver/utility/loading_gif/gif_player.html &&
-sudo ifconfig wlan0 up && wpa_cli -i wlan0 reconnect &&
-#sudo systemctl stop fingerscan.service &&
-#sleep 10 &&
-#sudo systemctl start fingerscan.service &&
-sleep 30 &&
-pkill -o chromium
+sudo ifconfig wlan0 up && wpa_cli -i wlan0 reconnect && sudo systemctl stop fingerscan.service && sleep 10 && sudo systemctl start fingerscan.service && sleep 30 && pkill -o chromium
 
