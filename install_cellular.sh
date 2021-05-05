c7cd2b6a1ba2a0e64a8303f3b3ce4e16d74c4c05
b.meta <metasound00@gmail.com>
2021-05-05 16:24:27 +0700

    fix bug

--- install_cellular.sh
+++ install_cellular.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get install usb-modeswitch ppp
-cd home/pi/ && mkidr 4Gmodem && cd /home/pi/
+cd /home/pi/ && mkidr 4Gmodem
 sudo cp /home/pi/ct-driver/cellular/umtskeeper.tar.gz /home/pi/4Gmodem/
 md5sum /home/pi/4Gmodem/umtskeeper.tar.gz
 tar -xzvf /home/pi/4Gmodem/umtskeeper.tar.gz
