0da5cb8302ccc73bcd435764e841cbc917ed4962
b.meta <metasound00@gmail.com>
2021-05-11 02:56:27 +0700

    update sh

--- start_logo.sh
+++ start_logo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo ifconfig wlan0 up && wpa_cli -i wlan0 reconnect && sudo systemctl stop fingerscan.service && sleep 10 && sudo systemctl start fingerscan.service && sleep 30 && pkill -o chromium && DISPLAY=:0 pcmanfm --set-wallpaper /home/pi/ct-driver/utility/splash/splash.png
+DISPLAY=:0 pcmanfm --set-wallpaper /home/pi/ct-driver/utility/splash/ffx_white_ct200.png && sudo ifconfig wlan0 up && wpa_cli -i wlan0 reconnect && sudo systemctl stop fingerscan.service && sleep 10 && sudo systemctl start fingerscan.service && sleep 30 && pkill -o chromium && DISPLAY=:0 pcmanfm --set-wallpaper /home/pi/ct-driver/utility/splash/splash.png
 
